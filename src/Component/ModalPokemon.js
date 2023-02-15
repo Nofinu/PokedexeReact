dcf48ca379d1d85dd3e99daf4a290738b5527f22
Léo Lhomme <leolhommeo@gmail.com>
2023-02-15 10:59:15 +0100

    ajout des encounters

--- src/Component/ModalPokemon.js
+++ src/Component/ModalPokemon.js
@@ -1,8 +1,10 @@
+import { useEffect, useState } from "react";
+import { getInfoPokemon } from "../services/data.services";
 
 export function ModalPokemon (props){
   const importAll=(r)=> {
     let images = {};
-    r.keys().forEach((item, index) => { 
+    r.keys().forEach((item) => { 
       images[item.replace('./', '').replace('.png','')] = r(item); 
     });
     return images
@@ -12,8 +14,18 @@ export function ModalPokemon (props){
     props.FermetureModalPokemon()
   }
 
+  const [PokemonEncounter,setPokemonEncounter]=useState()
+  const urlPokemonEncounter = "https://pokeapi.co/api/v2/pokemon/"+props.pokemonName+"/encounters"
+
+  useEffect(()=>{
+    getInfoPokemon(urlPokemonEncounter).then(res => setPokemonEncounter(res.data))
+    return()=> setPokemonEncounter([])
+    },[urlPokemonEncounter])
+
   const images = importAll( require.context('../asset', false, /\.(png|jpe?g|svg)$/))
   const idpokemon = props.modalPokemon.info.species.url.split("/")
+
+  console.log(PokemonEncounter)
   return(
     <div className="modalBackground">
       <div className="modalContant">
@@ -26,10 +38,26 @@ export function ModalPokemon (props){
           <p>{props.pokemonName} n°: {idpokemon[6]}</p>
           <img src={props.modalPokemon.info.sprites.front_default} alt="un truc"></img>
         </div>
+
         <div className="containerPokemonType">
           {
-            props.modalPokemon.info.types.map((elem,i) =><img src={images["Type_"+elem.type.name]} key={i}></img>)
+            props.modalPokemon.info.types.map((elem,i) =><img src={images["Type_"+elem.type.name]} key={i} alt="pokemonDisplay"></img>)
           }
+          <h4>Abilities :</h4>
+          <ul>
+            {
+              props.modalPokemon.info.abilities.map((elem,i)=><li key={i}>{elem.ability.name}</li>)
+            }
+          </ul>
+        </div>
+
+        <div className="containerEncounter">
+          <h4>Encounters :</h4>
+            <ul>
+            {
+              PokemonEncounter&& PokemonEncounter.map((elem,i)=><li key={i}>{elem.location_area.name}</li>)
+            }
+            </ul>
         </div>
 
         </div>
