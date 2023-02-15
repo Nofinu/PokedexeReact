523d1503e8dd697990115182277accf5dcbd7582
Léo Lhomme <leolhommeo@gmail.com>
2023-02-15 16:08:50 +0100

    ajout de css + chargement des encouter

--- src/Component/ModalPokemon.js
+++ src/Component/ModalPokemon.js
@@ -14,17 +14,34 @@ export function ModalPokemon (props){
     props.FermetureModalPokemon()
   }
 
-  const [PokemonEncounter,setPokemonEncounter]=useState()
+  const searchEncounter=()=>{
+    if(!testUrl){
+      setTestUrl(true)
+      getInfoPokemon(urlPokemonEncounter).then(res => {
+        if(res.data.length === 0){
+          setPokemonEncounter([{location_area:{name:"No encounter..."}}])
+        }
+        else{
+          setPokemonEncounter(res.data)
+        }
+      })
+    }
+  }
+
+  const [PokemonEncounter,setPokemonEncounter]=useState([])
+  const [testUrl,setTestUrl] = useState(false)
   const urlPokemonEncounter = "https://pokeapi.co/api/v2/pokemon/"+props.pokemonName+"/encounters"
 
   useEffect(()=>{
-    getInfoPokemon(urlPokemonEncounter).then(res => setPokemonEncounter(res.data))
-    return()=> setPokemonEncounter([])
-    },[urlPokemonEncounter])
+    searchEncounter()
+    return()=> {
+      setPokemonEncounter([])
+      setTestUrl(false)
+    }
+    },[])
 
   const images = importAll( require.context('../asset', false, /\.(png|jpe?g|svg)$/))
   const idpokemon = props.modalPokemon.info.species.url.split("/")
-
   console.log(PokemonEncounter)
   return(
     <div className="modalBackground">
@@ -33,17 +50,18 @@ export function ModalPokemon (props){
         <div className="headerModalPokedex">
         <button className="fa-sharp fa-solid fa-xmark" onClick={onClickBtn}> </button>
         </div>
-
+        <div className="rondBlanc"></div>
         <div className="containerPokemonDisplay">
           <p>{props.pokemonName} n°: {idpokemon[6]}</p>
           <img src={props.modalPokemon.info.sprites.front_default} alt="un truc"></img>
         </div>
 
         <div className="containerPokemonType">
+          <h4 className="titleTypes">Types :</h4>
           {
             props.modalPokemon.info.types.map((elem,i) =><img src={images["Type_"+elem.type.name]} key={i} alt="pokemonDisplay"></img>)
           }
-          <h4>Abilities :</h4>
+          <h4 className="titleAbilities">Abilities :</h4>
           <ul>
             {
               props.modalPokemon.info.abilities.map((elem,i)=><li key={i}>{elem.ability.name}</li>)
@@ -55,8 +73,10 @@ export function ModalPokemon (props){
           <h4>Encounters :</h4>
             <ul>
             {
-              PokemonEncounter&& PokemonEncounter.map((elem,i)=><li key={i}>{elem.location_area.name}</li>)
-            }
+              PokemonEncounter.length >0 ? PokemonEncounter.map((elem,i)=><li key={i}>{elem.location_area.name}</li>)
+              :
+              <li>Loading ...</li>
+              }
             </ul>
         </div>
 
